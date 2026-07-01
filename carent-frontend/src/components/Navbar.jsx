a1402f93a690502c3eff25aada3ec5bf49cd9543
Madue Unity <madue.unity.9@gmail.com>
2026-07-01 10:11:08 +0100

    fix

--- carent-frontend/src/components/Navbar.jsx
+++ carent-frontend/src/components/Navbar.jsx
@@ -73,7 +73,7 @@ export default function Navbar() {
         {/* Right side */}
         <div className="flex items-center gap-4">
           {user && (
-            <div className="hidden lg:flex items-center gap-3 rounded-full  bg-gray-50 px-3 py-2 shadow-sm">
+            <div className="hidden lg:flex items-center gap-3 rounded-full  bg-gray-50 px-4 py-2 shadow-sm">
               <img
                 src={getAvatarUrl(user)}
                 alt={user.name || user.email || "User avatar"}
